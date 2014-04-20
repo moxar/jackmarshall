25282b05100a19b5bdd8ab8358f4d50924958367
Alexandre Thomas <alexandre.thomas@outlook.fr>
2014-04-20 18:59:47 +0200

    Reorganized some files.

---
+++ app/views/tournaments/ranking.php
@@ -0,0 +1,30 @@
+			<section id="rankingSection">
+				<h1>Classement</h1>
+				<table class="table table-striped table-condensed table-hover">
+					<thead>
+						<tr>
+							<th>Joueur</th>
+							<th>VP</th>
+							<th>CP</th>
+							<th>DP</th>
+							<th>SOS</th>
+						</tr>
+					</thead>
+					<tbody>
+<?php
+foreach($players as $player)
+{
+?>
+					<tr>
+						<td><?php echo $player->name; ?></td>
+						<td><?php echo $player->victory; ?></td>
+						<td><?php echo $player->control; ?></td>
+						<td><?php echo $player->destruction; ?></td>
+						<td><?php echo $player->sos; ?></td>
+					</tr>
+<?php
+}
+?>
+					</tbody>
+				</table>
+			</section>
